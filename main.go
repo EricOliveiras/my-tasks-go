409926bc218f4a4f0ab283075f1bc130a63938ce
Eric Oliveira <oiericoi@hotmail.co>
2023-07-04 13:48:41 -0300

    feat: add main file

---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"github/ericoliveiras/basic-crud-go/controllers"
+	"github/ericoliveiras/basic-crud-go/models"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	models.ConnectDatabase()
+
+	router.POST("/user", controllers.CreateUser)
+
+	router.Run("localhost:8080")
+}
