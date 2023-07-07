add397e1f786c142f0c8003baef211275133d4e9
Eric Oliveira <oiericoi@hotmail.co>
2023-07-07 18:53:47 -0300

    feat: add password field in user model

--- models/user.go
+++ models/user.go
@@ -3,10 +3,11 @@ package models
 import "time"
 
 type User struct {
-	ID        string    `json:"id" gorm:"primaryKey"`
+	ID        string    `json:"id" gorm:"primaryKey;unique"`
 	FirstName string    `json:"first_name"`
 	LastName  string    `json:"last_name"`
 	Email     string    `json:"email" gorm:"unique"`
+	Password  string    `json:"password"`
 	CreatedAt time.Time `json:"created_at"`
 	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
