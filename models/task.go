4f3f2b1e283cb4ad3092204c7e630c59ed5658e2
Eric Oliveira <oiericoi@hotmail.co>
2023-07-12 20:45:30 -0300

    feat: add task model

---
+++ models/task.go
@@ -0,0 +1,13 @@
+package models
+
+import "time"
+
+type Task struct {
+	ID          string    `json:"id" gorm:"primaryKey;unique"`
+	UserID      string    `json:"user_id"`
+	Title       string    `json:"title"`
+	Description string    `json:"description"`
+	Finished    bool      `json:"finished"`
+	CreatedAt   time.Time `json:"created_at"`
+	UpdatedAt   time.Time `json:"updated_at" gorm:"autoUpdateTime"`
+}
