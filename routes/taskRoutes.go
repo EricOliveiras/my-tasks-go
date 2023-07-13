0aa1540f520fbce2397a76ce1dc424fc7379efca
Eric Oliveira <oiericoi@hotmail.co>
2023-07-12 21:14:50 -0300

    feat: add task router

---
+++ routes/taskRoutes.go
@@ -0,0 +1,13 @@
+package routes
+
+import (
+	"github/ericoliveiras/basic-crud-go/controllers"
+
+	"github.com/gin-gonic/gin"
+)
+
+func TaskRouter(router *gin.Engine) {
+	router.POST("/task", controllers.CreateTask)
+	router.GET("/task", controllers.ReadTasks)
+	router.GET("/task/:id", controllers.ReadTask)
+}
