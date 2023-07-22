297621b2ad20678832dc18010ab2b2e516517ac4
Eric Oliveira <oiericoi@hotmail.com>
2023-07-22 13:08:17 -0300

    feat: add auth middleware in protected routes

--- routes/userRoutes.go
+++ routes/userRoutes.go
@@ -2,14 +2,20 @@ package routes
 
 import (
 	"github/ericoliveiras/basic-crud-go/controllers"
+	"github/ericoliveiras/basic-crud-go/middleware"
 
 	"github.com/gin-gonic/gin"
 )
 
 func UserRouter(router *gin.Engine) {
-	router.POST("/user", controllers.CreateUser)
-	router.GET("/user", controllers.ReadUsers)
-	router.GET("/user/:id", controllers.ReadUser)
-	router.PUT("/user/:id", controllers.UpdateUser)
-	router.DELETE("/user/:id", controllers.DeleteUser)
+	userRoutes := router.Group("/user")
+
+	userRoutes.POST("", controllers.CreateUser)
+
+	userRoutes.Use(middleware.AuthMiddleware)
+	{
+		userRoutes.GET("/me", controllers.Me)
+		userRoutes.PUT("", controllers.UpdateUser)
+		userRoutes.DELETE("", controllers.DeleteUser)
+	}
 }
