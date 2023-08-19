15ace3c253abdb8d1d918a13a81b0e182cb72e5b
Eric Oliveira <oiericoi@hotmail.com>
2023-08-19 12:41:27 -0300

    refactor: modify database

--- database/db.go
+++ database/db.go
@@ -4,6 +4,7 @@ import (
 	"github/ericoliveiras/basic-crud-go/config"
 	"github/ericoliveiras/basic-crud-go/models"
 	"log"
+	"time"
 
 	"github.com/joho/godotenv"
 	"gorm.io/driver/postgres"
@@ -20,10 +21,27 @@ func ConnectDatabase() {
 	}
 
 	dsn := config.DbURL()
-	DB, err = gorm.Open(postgres.Open(dsn), &gorm.Config{})
 
-	if err != nil {
-		log.Panic("Failed to connect to database")
+	maxRetries := 5
+	retryInterval := time.Second * 5
+
+	var dbErr error
+
+	for retries := 0; retries < maxRetries; retries++ {
+		DB, dbErr = gorm.Open(postgres.Open(dsn), &gorm.Config{})
+		if dbErr == nil {
+			break
+		}
+
+		log.Printf("Attempt %d of %d: Error connecting to the database: %s\n", retries+1, maxRetries, dbErr)
+		if retries < maxRetries-1 {
+			log.Printf("Retrying in %s...\n", retryInterval)
+			time.Sleep(retryInterval)
+		}
+	}
+
+	if dbErr != nil {
+		log.Panic("Failed to connect to the database after multiple attempts.")
 	}
 
 	DB.AutoMigrate(&models.User{}, &models.Task{})
