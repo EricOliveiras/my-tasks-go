33faf72f121a36b591dd5c49d2a978cc310f908f
Eric Oliveira <oiericoi@hotmail.com>
2023-08-25 10:11:23 -0300

    feat: add db config

---
+++ internal/config/db.go
@@ -0,0 +1,22 @@
+package config
+
+import "os"
+
+type DBConfig struct {
+	User     string
+	Password string
+	Driver   string
+	Name     string
+	Host     string
+	Port     string
+}
+
+func LoadDBConfig() DBConfig {
+	return DBConfig{
+		User:     os.Getenv("DB_USER"),
+		Password: os.Getenv("DB_PASS"),
+		Name:     os.Getenv("DB_NAME"),
+		Host:     os.Getenv("DB_HOST"),
+		Port:     os.Getenv("DB_PORT"),
+	}
+}
