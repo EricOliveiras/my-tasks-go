4d88e094b2dc9b292ea57b94206c8a6c2d524ac2
Eric Oliveira <oiericoi@hotmail.com>
2023-08-30 13:36:01 -0300

    refactor: modify user model

--- internal/models/user.go
+++ internal/models/user.go
@@ -1,15 +1,14 @@
 package models
 
-import (
-	"gorm.io/gorm"
-)
+import "time"
 
 type User struct {
-	gorm.Model
-	ID        string `json:"id" gorm:"primaryKey;unique"`
-	FirstName string `json:"first_name"`
-	LastName  string `json:"last_name"`
-	Email     string `json:"email" gorm:"unique"`
-	Password  string `json:"password"`
-	Task      []Task `gorm:"foreignKey:UserId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	ID        string    `json:"id" gorm:"primaryKey;unique"`
+	FirstName string    `json:"first_name"`
+	LastName  string    `json:"last_name"`
+	Email     string    `json:"email" gorm:"unique"`
+	Password  string    `json:"password"`
+	Task      []Task    `gorm:"foreignKey:UserId;constraint:OnUpdate:CASCADE,OnDelete:SET NULL;"`
+	CreatedAt time.Time `json:"created_at"`
+	UpdatedAt time.Time `json:"updated_at" gorm:"autoUpdateTime"`
 }
