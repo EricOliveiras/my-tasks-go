57c422a353307dcbeaff9f21da89219674769bb7
Eric Oliveira <oiericoi@hotmail.com>
2023-08-30 13:46:05 -0300

    feat: add user requests

---
+++ internal/requests/user_requests.go
@@ -0,0 +1,13 @@
+package requests
+
+type BasicAuth struct {
+	Email    string `json:"email"`
+	Password string `json:"password"`
+}
+
+type CreateUserRequest struct {
+	ID        string `json:"id"`
+	FirstName string `json:"first_name"`
+	LastName  string `json:"last_name"`
+	BasicAuth
+}
