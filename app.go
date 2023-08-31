9530d45e4c6e3a041411378b751c3f4905be207c
Eric Oliveira <oiericoi@hotmail.com>
2023-08-31 11:17:32 -0300

    refactor: modify app.go file

--- app.go
+++ app.go
@@ -2,12 +2,15 @@ package application
 
 import (
 	"github/ericoliveiras/basic-crud-go/internal/config"
+	"github/ericoliveiras/basic-crud-go/internal/routes"
 	"github/ericoliveiras/basic-crud-go/internal/server"
 	"log"
 )
 
 func Start(cfg *config.Config) {
 	app := server.NewServer(cfg)
+
+	routes.UserRoutes(app)
 
 	err := app.Start(cfg.HTTP.Port)
 	if err != nil {
