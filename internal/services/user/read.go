39fec2b65166e0f8a7cb26c3a3ad87ede454bad9
Eric Oliveira <oiericoi@hotmail.com>
2023-09-20 20:17:34 -0300

    refactor: modify read user service

--- internal/services/user/read.go
+++ internal/services/user/read.go
@@ -1,12 +1,24 @@
 package services
 
-import "github/ericoliveiras/basic-crud-go/internal/models"
+import (
+	"github/ericoliveiras/basic-crud-go/internal/responses"
+)
 
-func (s *UserService) Read(id string) (models.User, error) {
+func (s *UserService) Read(id string) (responses.UserResponse, error) {
+	var userResponse responses.UserResponse
 	user, err := s.UserRepository.GetById(id)
 	if err != nil {
-		return models.User{}, err
+		return responses.UserResponse{}, err
 	}
 
-	return user, nil
+	userResponse = responses.UserResponse{
+		ID:        user.ID,
+		FirstName: user.FirstName,
+		LastName:  user.LastName,
+		Email:     user.Email,
+		CreatedAt: user.CreatedAt,
+		UpdatedAt: user.UpdatedAt,
+	}
+
+	return userResponse, nil
 }
