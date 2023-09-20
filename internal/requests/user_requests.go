0ec20800ac5ee79c87f95a51859e4fc3f6d6db8f
Eric Oliveira <oiericoi@hotmail.com>
2023-09-20 20:11:17 -0300

    refactor: modify user requests

--- internal/requests/user_requests.go
+++ internal/requests/user_requests.go
@@ -1,14 +1,5 @@
 package requests
 
-type BasicAuth struct {
-	Email    string `json:"email"`
-	Password string `json:"password"`
-}
-
-type LoginRequest struct {
-	BasicAuth
-}
-
 type CreateUserRequest struct {
 	ID        string `json:"id"`
 	FirstName string `json:"first_name"`
