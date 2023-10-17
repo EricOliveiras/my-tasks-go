4e34effe4b2b7226be0654c6555b7470014ea9c1
Eric Oliveira <oiericoi@hotmail.com>
2023-10-17 14:07:45 -0300

    feat: add cors config

---
+++ internal/config/cors.go
@@ -0,0 +1,16 @@
+package config
+
+import (
+	"github.com/gin-contrib/cors"
+	"github.com/gin-gonic/gin"
+)
+
+func LoadCorsConfig() gin.HandlerFunc {
+	return cors.New(cors.Config{
+		AllowAllOrigins:  true,
+		AllowMethods:     []string{"*"},
+		AllowHeaders:     []string{"*"},
+		ExposeHeaders:    []string{"Origin", "Content-Length", "Content-Type"},
+		AllowCredentials: true,
+	})
+}
