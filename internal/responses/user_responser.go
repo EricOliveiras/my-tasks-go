93c4a4b418de081dd32ac03d87c647af0e66289b
Eric Oliveira <oiericoi@hotmail.com>
2023-10-17 14:08:47 -0300

    refactor: modify user response

--- internal/responses/user_responser.go
+++ internal/responses/user_responser.go
@@ -1,12 +1,16 @@
 package responses
 
-import "time"
+import (
+	"github/ericoliveiras/basic-crud-go/internal/models"
+	"time"
+)
 
 type UserResponse struct {
-	ID        string    `json:"id"`
-	FirstName string    `json:"first_name"`
-	LastName  string    `json:"last_name"`
-	Email     string    `json:"email"`
-	CreatedAt time.Time `json:"created_at"`
-	UpdatedAt time.Time `json:"updated_at"`
+	ID        string        `json:"id"`
+	FirstName string        `json:"first_name"`
+	LastName  string        `json:"last_name"`
+	Email     string        `json:"email"`
+	Tasks     []models.Task `json:"tasks"`
+	CreatedAt time.Time     `json:"created_at"`
+	UpdatedAt time.Time     `json:"updated_at"`
 }
